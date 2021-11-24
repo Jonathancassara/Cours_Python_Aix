53ad4ff77eec5e4e916232ddda8d9ac746f16af8
JONATHAN CASSARA <37684306+Jonathancassara@users.noreply.github.com>
2021-11-24 23:32:28 +0100

    Update Menu
    Creation Class fichier csv

--- menu.py
+++ menu.py
@@ -22,54 +22,64 @@ import random
 # datefmt Format Jours Mois Année , Heure Minute Seconde
 logging.basicConfig(filename="logfilename.log", encoding='utf-8', level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
 
-#Défintion pour le choix Importer un annuaire
-def option1():
-    # Demande à l'user le nom du fichier CSV à importer.
-    fichier_csv = input("Quel est le nom de votre fichier annuaire ? " +'\x1b[6;37;41m' + " Uniquement au format CSV " + '\x1b[0m')
-    os.path.isfile(fichier_csv)
-    file_extension = os.path.splitext(fichier_csv)[1]
-    #Vérification de l'extention du fichier en .CSV
-    if file_extension.lower() == ".csv":
-        try:
-            #si fichier présent et format correct alors création    
-            with open(fichier_csv , 'r') as file:
-                logging.info('format de fichier correct')
-                print("importation réussi")
-        except IOError:
-                #Si fichier absent et format correct, on demande si création du fichier
-                print('\x1b[6;30;41m' + "Fichier Absent" + '\x1b[0m')
-                answer = input('\x1b[0;30;44m' + "Voulez-vous créer le fichier ? oui ou non :" + '\x1b[0m').lower()
-                if answer == 'oui':
-                    with open(fichier_csv,'w',newline='') as fichiercsv:
-                        writer=csv.writer(fichiercsv)
-                        writer.writerow(['Nom', 'Prénom', 'Téléphone'])
-                        print("Création réussi")
-                #si le choix est non, on retourne au menu principal
-                elif answer == 'non':
-                    print("Aucune Création de fichier")
-                return               
-    else:
-        if file_extension.lower() != ".csv":
-            logging.error('format de fichier incorrect')
-            print('\x1b[6;30;41m' + "erreur de format de fichier" + '\x1b[0m')
-    return
+class Fichiercsv:
+    #Défintion pour le choix Importer un annuaire
+    def option1():
+        # Demande à l'user le nom du fichier CSV à importer.
+        fichier_csv = input("Quel est le nom de votre fichier annuaire ? " +'\x1b[6;37;41m' + " Uniquement au format CSV " + '\x1b[0m')
+        os.path.isfile(fichier_csv)
+        file_extension = os.path.splitext(fichier_csv)[1]
+        #Vérification de l'extention du fichier en .CSV
+        if file_extension.lower() == ".csv":
+            try:
+                #si fichier présent et format correct alors création    
+                with open(fichier_csv , 'r') as file:
+                    logging.info('format de fichier correct')
+                    print("importation réussi de :", fichier_csv)
+                    return
+            except IOError:
+                    #Si fichier absent et format correct, on demande si création du fichier
+                    print('\x1b[6;30;41m' + "Fichier Absent" + '\x1b[0m')
+                    answer = input('\x1b[0;30;44m' + "Voulez-vous créer le fichier ? oui ou non :" + '\x1b[0m').lower()
+                    if answer == 'oui':
+                        with open(fichier_csv,'w',newline='') as fichier_csv:
+                            writer=csv.writer(fichier_csv)
+                            writer.writerow(['Nom', 'Prénom', 'Téléphone'])
+                            print("Création réussi")
+                            logging.info('création du fichier')
+                    #si le choix est non, on retourne au menu principal
+                    elif answer == 'non':
+                        print("Aucune Création de fichier")
+                        logging.info('aucun fichier créé retour au menu')
+                    return fichier_csv            
+        else:
+            if file_extension.lower() != ".csv":
+                logging.error('format de fichier incorrect')
+                print('\x1b[6;30;41m' + "erreur de format de fichier" + '\x1b[0m')
+        return fichier_csv
 
-#Défintion pour le choix Chercher un utilisateur et afficher ses informations
-def option2(): 
-    print('\x1b[6;37;41m' + "REUSSI" + '\x1b[0m')
-    logging.info('REUSSI')
-           
-#Défintion pour le choix Ajouter un utilisateur
-def option3(importation = 'Agenda.csv'):
-    print('\x1b[6;37;41m' + "REUSSI" + '\x1b[0m')
-    logging.info('REUSSI')    
-        
-#Défintion pour le choix Exporter l’annuaire
-def option4():
-    print('\x1b[6;37;41m' + "REUSSI" + '\x1b[0m')
-    logging.info('REUSSI')
 
-#Fermeture du script avec suppression du fichier Agenda.csv
-def option5():
-    print('\x1b[6;37;41m' + "REUSSI" + '\x1b[0m')
-    logging.info('REUSSI')      
+
+    #Défintion pour le choix Chercher un utilisateur et afficher ses informations
+    def option2(fichier_csv ):
+        with open(fichier_csv , 'r') as read_obj:
+            fichier_csv  = reader(read_obj)
+            for row in fichier_csv :
+                print('\x1b[6;30;42m' + row[1],'\x1b[6;30;42m' +  row[2],'\x1b[6;30;42m' +  row[3]+ '\x1b[0m')
+                logging.info('Affichage des données')
+                logging.info('REUSSI')
+            
+    #Défintion pour le choix Ajouter un utilisateur
+    def option3(importation = 'Agenda.csv'):
+        print('\x1b[6;37;41m' + "REUSSI" + '\x1b[0m')
+        logging.info('REUSSI')    
+            
+    #Défintion pour le choix Exporter l’annuaire
+    def option4():
+        print('\x1b[6;37;41m' + "REUSSI" + '\x1b[0m')
+        logging.info('REUSSI')
+
+    #Fermeture du script avec suppression du fichier Agenda.csv
+    def option5():
+        print('\x1b[6;37;41m' + "REUSSI" + '\x1b[0m')
+        logging.info('REUSSI')      
