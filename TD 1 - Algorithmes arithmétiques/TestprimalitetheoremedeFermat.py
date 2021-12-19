29b42e40b06b3c576085d5aa026c06cf4110caf5
JONATHAN CASSARA <37684306+Jonathancassara@users.noreply.github.com>
2021-12-19 01:23:42 +0100

    Theoreme de fermat
    Theroeme de Fermat

---
+++ TD 1 - Algorithmes arithmétiques/TestprimalitetheoremedeFermat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# TD 1 - Algorithmes arithmétiques
+# Auteur : Jonathan CASSARA-GOHIER
+# Version 1 18/12/2021
+# copie et utilisation non autorisé
+# git@github.com:Jonathancassara/Cours_Python_Aix.git
+
+import math
+n = int(input("Votre chiffre : "))
+def test_Fermat(n : int, k : int) -> bool:
+    
+    
+    return
+
+def display_Fermat(n : int, k : int) -> None:
+    
+    return
