0cbcdfae64e1d4046142f7f15f9f811e0f86bc31
JONATHAN CASSARA <37684306+Jonathancassara@users.noreply.github.com>
2021-12-26 22:43:25 +0100

    Update TestprimalitetheoremedeFermat.py

--- TD 1 - Algorithmes arithmétiques/TestprimalitetheoremedeFermat.py
+++ TD 1 - Algorithmes arithmétiques/TestprimalitetheoremedeFermat.py
@@ -6,13 +6,15 @@
 # copie et utilisation non autorisé
 # git@github.com:Jonathancassara/Cours_Python_Aix.git
 
-import math
-n = int(input("Votre chiffre : "))
+import random
+n = input("N")
+k = input("K")
 def test_Fermat(n : int, k : int) -> bool:
+    a = random.randint(1,n-1)
+    if (a ** (n - 1)) % n == 1 and k >= 1 and k < n:
+        return True
+    else:
+        return False
     
-    
-    return
-
 def display_Fermat(n : int, k : int) -> None:
     
-    return
