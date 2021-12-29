8781e924fe8f05afb0b70e3198dfc6852f0f9fdd
JONATHAN CASSARA <37684306+Jonathancassara@users.noreply.github.com>
2021-12-29 22:37:53 +0100

    Create AlgorithmedEuclide.py

---
+++ TD 1 - Algorithmes arithmétiques/AlgorithmedEuclide.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# TD 1 - Algorithmes arithmétiques
+# Auteur : Jonathan CASSARA-GOHIER
+# Version 2.1 28/12/2021
+# copie et utilisation non autorisé
+# git@github.com:Jonathancassara/Cours_Python_Aix.git
+#Algorithme d’Euclide
