155d6e6128d0ac3250c20b7781f916015bea3b88
JONATHAN CASSARA <37684306+Jonathancassara@users.noreply.github.com>
2021-12-29 22:31:12 +0100

    Update décompositionfacteurspremiers.py

--- TD 1 - Algorithmes arithmétiques/décompositionfacteurspremiers.py
+++ TD 1 - Algorithmes arithmétiques/décompositionfacteurspremiers.py
@@ -8,3 +8,18 @@
 #Décomposition en facteurs premiers
 
 #def decompose(n :int) -> List[int]:
+def dec(N):
+    Resultat=[]
+    d=2
+    while N%d==0:
+        Resultat.append(d)
+        q=int(N/d)
+        N=q
+    d=3
+    while d<=N:
+        while N%d==0:
+            Resultat.append(d)
+            q=int(N/d)
+            N=q
+        d=d+2
+    return Resultat
