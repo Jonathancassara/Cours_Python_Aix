8b05dc1045f7bc00a70961330009a88bc93b48f7
JONATHAN CASSARA <37684306+Jonathancassara@users.noreply.github.com>
2021-12-31 21:31:40 +0100

    Create 8.4.2Calculpuissancedunentierversionoptimisee.py

---
+++ TD 1 - Algorithmes arithmétiques/8.4.2Calculpuissancedunentierversionoptimisee.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# TD 1 - Algorithmes arithmétiques
+# Auteur : Jonathan CASSARA-GOHIER
+# Version 1 31/12/2021
+# copie et utilisation non autorisé
+# git@github.com:Jonathancassara/Cours_Python_Aix.git
+#Calcul de la puissance d’un entier
+#méthode optimisée
+
+def pow_opt(x: int, n: int) -> int:
+    
