815dc1188aa844e72833c751d1e87f2eb11bb92a
JONATHAN CASSARA <37684306+Jonathancassara@users.noreply.github.com>
2022-01-01 15:59:32 +0100

    Update 8.6datedulendemain.py

---
+++ TD 1 - Algorithmes arithmétiques/8.6datedulendemain.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# TD 1 - Algorithmes arithmétiques
+# Auteur : Jonathan CASSARA-GOHIER
+# Version 1  01/01/2022
+# copie et utilisation non autorisé
+# git@github.com:Jonathancassara/Cours_Python_Aix.git
+#Date du lendemain
+
+def demain(date : str) -> str:
+    
