dac7d5843dfd069bbcfbef837d0c41f56096bc6b
JONATHAN CASSARA <37684306+Jonathancassara@users.noreply.github.com>
2022-01-01 23:52:42 +0100

    Update 8.6datedulendemain.py

--- TD 1 - Algorithmes arithmétiques/8.6datedulendemain.py
+++ TD 1 - Algorithmes arithmétiques/8.6datedulendemain.py
@@ -9,3 +9,36 @@
 
 def demain(date : str) -> str:
     
+    #Pseudo CODE
+    INPUT DAY
+    INPUT MONTH
+    INPUT YEAR
+    
+    BIXSEXTILE
+    IF a/4==0  and a%100 !=0  or a/400=0
+    is bix = True
+    else 
+    is bix = False
+    
+    IF m 1 . 3 . 5 . 7 . 8 . 10 .12 
+    num day 31 
+    else m = 2 and bix 
+    day = 29
+    elise m = 2 
+    num day = 28
+    else day = 30 
+    
+    
+    if j == day 
+    J = 1 
+    m = m +1 
+    else j + J +1 
+    
+    if m == 13
+    a = a + 1 
+    m = 1
+    
+    
+    print demain  J M A  
+    
+    
